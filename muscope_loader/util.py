a07a4fb167daffe05c83a251f9ddacdff6e1c593
Joshua Lynch <jklynch@email.arizona.edu>
2017-11-26 22:38:42 -0500

    remove session_ in favor of orminator.session_manager

--- muscope_loader/util.py
+++ muscope_loader/util.py
@@ -1,23 +1,8 @@
-from contextlib import contextmanager
 from itertools import zip_longest
 
 
 class FileNameException(Exception):
     pass
-
-
-@contextmanager
-def session_(session_class):
-    """Provide a transactional scope around a series of operations."""
-    session = session_class()
-    try:
-        yield session
-        session.commit()
-    except:
-        session.rollback()
-        raise
-    finally:
-        session.close()
 
 
 def grouper(iterable, n, fillvalue=None):
